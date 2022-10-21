374388c4ca75f766dbc57fb29ad4adb14a615ec5
SooBin An <jejupalette@naver.com>
2022-10-21 22:54:58 +0900

    design : 글씨체 update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,6 +12,13 @@ const MenuContainer = styled.div`
 `;
 
 const Menu = styled.div`
+  @font-face {
+    font-family: 'Cafe24Ssurround';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/Cafe24Ssurround.woff')
+      format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
   font-size: 20px;
   font-weight: 800;
   padding: 10px;
--- src/pages/DandD.js
+++ src/pages/DandD.js
@@ -37,6 +37,13 @@ const DNDListContianer = styled.ul`
   padding: 0;
 `;
 const DNDList = styled.div`
+  @font-face {
+    font-family: 'Cafe24Ssurround';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/Cafe24Ssurround.woff')
+      format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
   font-family: 'Cafe24Ssurround';
   color: rgb(67, 67, 67);
   width: 400px;
